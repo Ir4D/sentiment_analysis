0036b87ee52f51016bb16670a110eeaee1135672
Vitaly Fridman <finik073@gmail.com>
2020-12-24 02:03:28 +0400

    добавил текст комментариев, лайки комментариев, список групп

--- DATA/model.py
+++ DATA/model.py
@@ -3,12 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class post(db.Model):
+    group_url = db.Column(db.String, unique=True, nullable=False) #добавил на случай, если будем работать с разными группами, а не только с одной
     id = db.Column(db.Integer, primary_key=True)
     post_text = db.Column(db.Text, nullable=False)
-    url = db.Column(db.String, unique=True, nullable=False)
-    published = db.Column(db.DateTime, nullable=False)
-    num_likes = db.Column(db.Integer, nullable=False)
-    num_comments = db.Column(db.Integer, nullable=False)
+    post_published = db.Column(db.DateTime, nullable=False)
+    num_post_likes = db.Column(db.Integer, nullable=False)
+    num_post_comments = db.Column(db.Integer, nullable=False)
+    comment_text = db.Column(db.Text, nullable=False)
+    num_comm_likes = db.Column(db.Integer, nullable=False)
     
     def __repr__(self):
         return '<post {} {}>'.format(self.title, self.url)
